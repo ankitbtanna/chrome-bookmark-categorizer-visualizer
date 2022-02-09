685c006d89b5a4d54cf086b27aa145d3abc755fc
Ankit Tanna <atanna@slb.com>
2022-02-09 15:10:45 +0530

    initial commit

---
+++ apps/chrome-bookmark-categorizer-visualizer/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'chrome-bookmark-categorizer-visualizer-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'chrome-bookmark-categorizer-visualizer';
+}
